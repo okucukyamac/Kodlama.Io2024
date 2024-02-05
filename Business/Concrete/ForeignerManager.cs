63b9686c8c4f21a9d688761342d9c3bcd03c81a3
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2024-02-06 00:36:11 +0300

    Add project files.

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete;
+
+public class ForeignerManager:IApplicantService
+{
+    public void ApplyForMask(Person person)
+    {
+
+    }
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+
+    public bool CheckPerson(Person person)
+    {
+        return true;
+    }
+}
